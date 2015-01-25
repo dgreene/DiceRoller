067e70e0b382163b27523b63088f25ee6470333a
Tyler Caro <tylercaro@gmail.com>
2015-01-24 23:05:37 -0800

    Commit check

--- DiceRoller/src/diceroller/Main.java
+++ DiceRoller/src/diceroller/Main.java
@@ -5,9 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello Mudda");
+		System.out.println("Hello Fadda");
 		DiceRoller roller = new DiceRoller();
 		roller.getClass();
 		
